828cefe63d163cbc3cad63dbb13dff182dea917f
Federico Montejo <fmontejo@usf.edu>
2025-10-05 18:22:25 -0400

    Step 3 completed

--- cc_6.js
+++ cc_6.js
@@ -1,6 +1,6 @@
 //starting off with step 2
 
-class employee {
+class Employee {
     constructor(name, department) {
         this.name = name;
         this.department = department;
@@ -9,3 +9,16 @@ class employee {
         return `${this.name} works in ${this.department}`
     }
 }
+
+//step3 
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department)
+        this.teamSize = teamSize;
+    }
+    describe(){
+        return `${this.name} works at ${this.department} and handles ${this.teamSize} people`
+    }
+}
+
+//step 4
